aab8c4ab8122080ac2835c000c04c788a22c3efa
Empoliam <liamjwright@btinternet.com>
2022-08-02 18:52:08 +0100

    init

---
+++ src/main/java/dev/patchi/silk/Main.java
@@ -0,0 +1,12 @@
+package dev.patchi.silk;
+
+import org.lwjgl.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("LWJGL Version: " + Version.getVersion());
+    }
+
+}
